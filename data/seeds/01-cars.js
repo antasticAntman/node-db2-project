f87667cbc6f3b817460cee2c9915c45c885721df
Anthony Climer <anthonyclimer0925@gmail.com>
2023-05-02 21:34:43 -0400

    Done with db

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,29 @@
 // STRETCH
+const cars = [
+    {
+        vin: '1111111111111',
+        make: "toyota",
+        model: "prius",
+        mileage: 214000,
+        title: "clean",
+        transmission: "manual",
+    },
+    {
+        vin: '1111111161111',
+        make: "ford",
+        model: "mustang",
+        mileage: 250000,
+        title: "salvage",
+    },
+    {
+        vin: '1111111111111',
+        make: "toyota",
+        model: "corolla",
+        mileage: 5000,
+    },
+]
+
+exports.seed = async function(knex) {
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
